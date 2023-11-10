50474164c2c1f5916b5ff44ce6e18048ba265659
Edgaras Austinskas <eaustinskas@gmail.com>
2023-11-10 21:33:12 +0200

    Made part of logo

--- src/InputExample3.jsx
+++ src/InputExample3.jsx
@@ -1,13 +1,20 @@
+// Inside InputExample3.jsx
+
 import React from "react";
+import hexagonSVG from "./hexagon.svg"; // Import the SVG file
+import "./App.css"; // Import the main CSS file
 
 const InputExample3 = () => {
   return (
     <div className="form-section">
+      <div className="logo-container">
+        <img src={hexagonSVG} alt="Logo" className="logo" />
         <div className="logo-text">
-      <h3>Company</h3>
-      <h2>LOGO</h2>
-      <h4>Name</h4>
+          <h3>Company</h3>
+          <h1>LOGO</h1>
+          <h4>Name</h4>
         </div>
+      </div>
     </div>
   );
 };
